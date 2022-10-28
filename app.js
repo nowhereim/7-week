6eb6c5a64fb797d9c9bc0fa44fbb55f18c20f7d9
nowhereim <zyle1104@gmail.com>
2022-10-28 11:58:47 +0900

    hello

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+const cors = require("cors");
+const corsOption = {
+  origin: true,
+  credentials: true,
+};
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOption));
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
