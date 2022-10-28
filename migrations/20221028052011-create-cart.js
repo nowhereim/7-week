6545f89773ce34573ee673c7256f02180e0322bf
nowhereim <zyle1104@gmail.com>
2022-10-28 15:22:51 +0900

    feat cart add

---
+++ migrations/20221028052011-create-cart.js
@@ -0,0 +1,65 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "Carts",
+      {
+        cartId: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        productId: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
+        productImage: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        productName: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        price: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+        },
+        quantity: {
+          allowNull: false,
+          type: Sequelize.INTEGER,
+          defaultValue: 1,
+        },
+        Spare: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        category: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        address: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      },
+      {
+        charset: "utf8mb4",
+        collate: "utf8mb4_unicode_ci",
+      }
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Carts");
+  },
+};
