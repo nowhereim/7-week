942cbbb2528b8e1b18ec17eebba80ce53ae03fd6
nowhereim <zyle1104@gmail.com>
2022-10-31 00:04:00 +0900

    스웩띠

--- routes/questions.js
+++ routes/questions.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const middleware = require("../middlewares/auth-middleware");
 questioncontroller = require("../controllers/questions");
 questioncontroller = new questioncontroller();
 
-router.get("/", questioncontroller.getQuestion);
-router.get("/:questionId", questioncontroller.getQuestionOne);
-router.post("/", questioncontroller.createQuestion);
-router.put("/", questioncontroller.updateQuestion);
-router.delete("/", questioncontroller.deleteQuestion);
+router.get("/", middleware, questioncontroller.getQuestion);
+router.get("/:questionId", middleware, questioncontroller.getQuestionOne);
+router.post("/", middleware, questioncontroller.createQuestion);
+router.put("/", middleware, questioncontroller.updateQuestion);
+router.delete("/", middleware, questioncontroller.deleteQuestion);
 
 module.exports = router;
